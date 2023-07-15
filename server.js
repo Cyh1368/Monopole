933cb4b2f7cbf25e145cc8b1971e406919af9a34
Cheng-You Ho <chengyou1368@gmail.com>
2023-07-15 14:41:41 +0800

    server-compatible security settings, added 2 pdfs

--- server.js
+++ server.js
@@ -12,10 +12,12 @@ var fs = require('fs');
 const { log } = require('console');
 const app = express();
 const authExpire = 2 * 60; // Two minutes for developement stage, in SECONDS
-const cookieLength = 10 * 60 * 1000; // 10 min, in milliseconds
+const cookieLength = 24 * 60 * 60 * 1000; // 24 hr, in milliseconds
 const cryptoKey = "kA94fp@ki/2[]`jr-=`]"; // Should be kept secret in some way
 
 const questionCategories = ["me", "em", "th", "ap", "sr", "gr", "mo"];
+const port=3000;
+const hostname = '0.0.0.0'; // Bind to all available network interfaces
 
 async function authLoginToken(token){
 	// Input: login token, both stored in the client cookie and database
@@ -25,19 +27,19 @@ async function authLoginToken(token){
 		connection.query('SELECT UserID, Username FROM logintb WHERE loginToken = ?', [token], function(error, results, fields) {
 			// If there is an issue with the query, output the error
 			if (error) throw error;
-			// console.log("AUTHTOKEN result: ", results);
+			//console.log("AUTHTOKEN result: ", results);
 			var string=JSON.stringify(results);
 			var json =  JSON.parse(string);
-			console.log("AUTH json: ", json)
+			//console.log("AUTH json: ", json)
 			if (json[0]!=undefined) ret.UserID = json[0].UserID;
 			if (json[0]!=undefined) ret.Username = json[0].Username;
 			
 			resolve(ret);
-			// console.log("RET: ", ret);
+			//console.log("RET: ", ret);
 		});
 	});
 	let promiseValue = await sqlPromise;
-	// console.log("Promise value: ", promiseValue);
+	//console.log("Promise value: ", promiseValue);
 	return promiseValue;
 }
 
@@ -53,13 +55,13 @@ function setResponseCookies(res, jsonData){
 	now.setTime(expireTime);
 	res.cookie('clientUserID', jsonData.UserID, {
 		expires: now, // There's maybe a bug here because there's language-dependent result here like 台北標準時間
-		secure: true,
+		secure: false,
 		httpOnly: false, // As of 20230710 client-side js can't access cookie if set to true, making it less secure
 		sameSite: 'lax'
 	});
 	res.cookie('clientUsername', jsonData.Username, {
 		expires: now, // There's maybe a bug here because there's language-dependent result here like 台北標準時間
-		secure: true,
+		secure: false,
 		httpOnly: false, // As of 20230710 client-side js can't access cookie if set to true, making it less secure
 		sameSite: 'lax'
 	});
@@ -79,7 +81,7 @@ var generate_key = function() {
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Ch122iv0Console.log",
+  password: "Ch122iv0Console.log", // Change in server, this is not the server pswd
   database: "Monopole"
 });
 
@@ -96,18 +98,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', function(request, response) {
-	response.cookie("clientToken", "0");
-	// console.log("Return of auth: ", authLoginToken(request.cookies.clientToken));
+	//console.log("Return of auth: ", authLoginToken(request.cookies.clientToken));
 	let authResult;
 	(async () => {
 		authResult = await authLoginToken(request.cookies.clientToken)
 		response = setResponseCookies(response, authResult);
 		response.sendFile(path.join(__dirname + '/home.html'))
-		// console.log("IIFE log: ", authResult);
+		//console.log("IIFE log: ", authResult);
 	})()
 
-	// console.log(authLoginToken("blah"));
-	// console.log(request.cookies);
+	//console.log(authLoginToken("blah"));
+	//console.log(request.cookies);
     // response.sendFile(path.join(__dirname + '/home.html'));
 });
 
@@ -118,7 +119,7 @@ app.get('/pdfs', function(request, response) {
 		response = setResponseCookies(response, authResult);
 		response.sendFile(path.join(__dirname + '/pdfs.html'))
 	})()
-	// console.log("IIFE log: ", authResult);
+	//console.log("IIFE log: ", authResult);
 });
 
 app.get('/monopole', function(request, response) {
@@ -173,9 +174,9 @@ app.post('/auth', function(request, response) {
 		response = setResponseCookies(response, authResult);
 	})()
 
-    console.log("Authentication Start")
+    //console.log("Authentication Start")
 	// Capture the input fields
-    // console.log(request)
+    //console.log(request)
 	let username = request.body.username;
 	let password = request.body.password;
 	// Ensure the input fields exists and are not empty
@@ -187,26 +188,26 @@ app.post('/auth', function(request, response) {
 			if (error) throw error;
 			// If the account exists
 			if (results.length > 0) {
-                console.log("Auth result is not null")
+                //console.log("Auth result is not null")
 				// Authenticate the user
 				request.session.loggedin = true;
 				request.session.username = username;
 				// Redirect to home page\
-                console.log("Response as follows:");
-                console.log(response);
+                //console.log("Response as follows:");
+                //console.log(response);
 
 				// Update "expireTime" column
 				var current = Math.floor(Date.now() / 1000); // Storing the "seconds"
 				var expire = (current + authExpire) % 2147483647; // In "seconds", since millisecond accuracy is waste of database
 				// To prevent overflow, I take the modulus, though this may have a bug of 2147483647s period.
 
-				console.log("Expire time: ", expire);
+				//console.log("Expire time: ", expire);
 				connection.query('UPDATE logintb SET expireTime = ? WHERE Username = ?', [expire, username], function(error, results, fields){
 					if (error) throw error;
 				});
 
 				var loginToken = generate_key()
-				console.log("Login token: ", loginToken);
+				//console.log("Login token: ", loginToken);
 				connection.query('UPDATE logintb SET loginToken = ? WHERE Username = ?', [loginToken, username], function(error, results, fields){
 					if (error) throw error;
 				});
@@ -231,12 +232,12 @@ app.post('/auth', function(request, response) {
 				now.setTime(expireTime);
 				response.cookie('clientToken', loginToken, {
 					expires: now, // There's maybe a bug here because there's language-dependent result here like 台北標準時間
-					secure: true,
-					httpOnly: true, // So that client can't see the cookie
+					secure: false,
+					httpOnly: false, // So that client can't see the cookie
 					sameSite: 'lax'
 				});
 				response.redirect('/monopole');
-                console.log("Directed home.");
+                //console.log("Directed home.");
                 
 			} else {
 				response.send('Incorrect Username and/or Password!');
@@ -258,12 +259,12 @@ app.post('/register', function(request, response) {
 		response = setResponseCookies(response, authResult);
 	})()
 
-    console.log("Registration Start")
+    //console.log("Registration Start")
 	// Capture the input fields
 	let username = request.body.username;
 	let password1 = request.body.password1;
 	let password2 = request.body.password2;
-    console.log("Registering with " + username + ", " + password1 + ", " + password2);
+    //console.log("Registering with " + username + ", " + password1 + ", " + password2);
 	// Ensure the input fields exists and are not empty
 	if (username && password1) {
 		if (password1!=password2){
@@ -277,10 +278,10 @@ app.post('/register', function(request, response) {
 			connection.query(sql, function(error, results, fields) {
 				// If there is an issue with the query, output the error
 				if (error){
-					// console.log("ERROR in register()");
+					//console.log("ERROR in register()");
 					throw error;
 				} 
-				console.log("Result is", results)
+				//console.log("Result is", results)
 				if (results.serverStatus==2) {
 					response.send("Registration completed! <a href='/monopoleLogin'>Login</a> here." )
 					response.end();
@@ -296,8 +297,6 @@ app.post('/register', function(request, response) {
 	}
 });
 
-app.listen(8080);
-
 app.post("/askSubmit", function(request, response){
 	let authResult, username="not checked", userid="not checked";
 	(async () => {
@@ -306,20 +305,20 @@ app.post("/askSubmit", function(request, response){
 		username = authResult.Username;
 		userid = authResult.UserID;
 
-		console.log(username, userid)
+		//console.log(username, userid)
 		if (username=="undefined" || userid=="undefined" || !username || !userid){
-			console.log("Null credentials");
+			//console.log("Null credentials");
 			response.send("You must log in before asking a question. <a href='/monopoleLogin'>Login</a> here.");
 			response.end();
 		} else {
 			let body = request.body;
-			console.log("Request body: ", body); // { title: 'ewh', main: 'wehw', mech: '1', sr: '1' }
+			//console.log("Request body: ", body); // { title: 'ewh', main: 'wehw', mech: '1', sr: '1' }
 			let categoryString = "";
 			for (category of questionCategories){
-				// console.log("cat", category, body[category]);
+				//console.log("cat", category, body[category]);
 				if (body[category]) categoryString+=category+"|"
 			}
-			// console.log(categoryString);
+			//console.log(categoryString);
 
 			// Fixing time overflow
 			var current = Math.floor(Date.now() / 1000) % 2147483647; // Storing the "seconds"
@@ -342,14 +341,14 @@ app.post("/sendComment", function(request, response){
 		username = authResult.Username;
 		userid = authResult.UserID;
 
-		console.log(username, userid);
+		//console.log(username, userid);
 		if (username=="undefined" || userid=="undefined" || !username || !userid){
-			console.log("Null credentials");
+			//console.log("Null credentials");
 			response.send({'status': 0});
 			response.end();
 		} else {
 			let body = request.body;
-			console.log("Request body: ", body); // { title: 'ewh', main: 'wehw', mech: '1', sr: '1' }
+			//console.log("Request body: ", body); // { title: 'ewh', main: 'wehw', mech: '1', sr: '1' }
 			
 			// Fixing time overflow
 			var commentTimeSec = Math.floor(body.CommentTime / 1000) % 2147483647;
@@ -381,7 +380,7 @@ app.get('/getRecentQuestions', function(request, response) {
 				QuestionID: item.QuestionID
 			});
 		}
-		console.log(clientResult);
+		//console.log(clientResult);
 		response.send(clientResult);
 		response.end();
 	});
@@ -390,7 +389,7 @@ app.get('/getRecentQuestions', function(request, response) {
 app.get('/getQuestionComments', function(request, response) {
 	let clientResult = [];
 	quesID = request.query.quesid;
-	console.log(quesID);
+	//console.log(quesID);
 	connection.query('SELECT * FROM cmntb WHERE QuestionID = ? ORDER BY PostTime DESC LIMIT 20', [quesID], function(error, results, fields) {
 		// If there is an issue with the query, output the error
 		if (error) throw error;
@@ -403,7 +402,7 @@ app.get('/getQuestionComments', function(request, response) {
 				QuestionID: item.QuestionID
 			});
 		}
-		console.log("getComments: ", clientResult);
+		//console.log("getComments: ", clientResult);
 		response.send(clientResult);
 		response.end();
 	});
@@ -424,8 +423,8 @@ app.get('/getQuestionByID', function(request, response) {
 			AskTime: secondsToUTC(item.AskTime),
 			QuestionID: item.QuestionID
 		};
-		console.log("questionR", item);
-		console.log(clientResult);
+		//console.log("questionR", item);
+		//console.log(clientResult);
 		response.send(clientResult);
 		response.end();
 	});
@@ -450,7 +449,7 @@ app.get('/viewQuestion', function(request, response) {
 		
 			response.cookie('viewQuestionID', quesID, {
 				expires: now, // There's maybe a bug here because there's language-dependent result here like 台北標準時間
-				secure: true,
+				secure: false,
 				httpOnly: false, // As of 20230710 client-side js can't access cookie if set to true, making it less secure
 				sameSite: 'lax'
 			});
@@ -460,3 +459,6 @@ app.get('/viewQuestion', function(request, response) {
 	}
 })
 
+app.listen(port, hostname, () => {
+	console.log(`Server running at http://localhost:${port}`);
+});
